abb3e6ad05b1f62295e471f85939e4fc3301356a
oladee <deemajor230600@gmail.com>
2023-10-24 01:38:55 +0100

    included assets  and install browser router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Home = ({names}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Home.propTypes = {
+ names: PropTypes.string
+}
+
+export default Home
