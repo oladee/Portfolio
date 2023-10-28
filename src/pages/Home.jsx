4917cf7a6b6395236beebc858383e871ec17045d
oladee <deemajor230600@gmail.com>
2023-10-28 11:29:08 +0100

    finshed with card template, working on respnsiveness of the list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 import displaypic from '../assets/oladee.jpg'
 import { stack } from "../stack";
+import previewIcon from '../assets/akar-icons_link-chain.svg'
+import gitIcon from '../assets/akar-icons_github-fill.svg'
+import { data } from "../data";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -18,7 +22,7 @@ const Home = () => {
       <section className="mb-20">
         <div className="flex flex-col justify-center items-center">
         <h3 className="text-3xl md:text-5xl font-bold pb-3">My Tech Stack</h3>
-        <p className="md:text-2xl pb-16">Technologies I've been Working with recently</p>
+        <p className="md:text-2xl pb-16">Technologies I&apos;ve been Working with recently</p>
         </div>
         <div className="flex ml-[15%] lg:ml-0">
         <div className="flex gap-20 flex-wrap">
@@ -29,11 +33,11 @@ const Home = () => {
       <section>
       <div className="flex flex-col justify-center items-center">
         <h3 className="text-3xl md:text-5xl font-bold pb-3">Projects</h3>
-        <p className="md:text-2xl pb-16">Things I've built so far</p>
+        <p className="md:text-2xl pb-16">Things I&apos;ve built so far</p>
         </div>
         <div className="flex lg:justify-normal justify-center lg:ml-0">
         <div className="flex gap-20">
-           <Card/>
+           {data.map(x => <Card key={x.title} image={x.image} title={x.title}/>)}
         </div>
         </div>
       </section>
@@ -46,15 +50,28 @@ Home.propTypes = {
 };
 export default Home;
 
-export const Card = ()=> {
-  return(<div className="border h-[350px] w-[250px]">
-    <div >
-
+export const Card = ({image, title, description, codeStack})=> {
+  return(<div className=" h-[390px] w-[250px] bg-[#363636] rounded-2xl">
+    <div className="h-[35%]">
+      <img src={image} alt="" className="w-full h-full object-cover rounded-t-2xl" />
     </div>
-    <div>
-
+    <div className="p-4">
+      <h3 className="font-medium text-xl pb-2">{title}</h3>
+      <p className=" line-clamp-4 ">{description}</p>
+      <p className="pt-2 pb-3">Tech stack : <span className="text-xs">{codeStack}</span></p>
+      <div className="flex justify-between">
+        <Link className="flex items-center"><img src={previewIcon} alt="" /> <span className="text-sm">Live Preview</span> </Link>
+        <Link className="flex items-center gap-1" ><img src={gitIcon} alt="" className="w-5" /> <span className="text-sm">View Code</span> </Link>
+      </div>
     </div>
   </div>)
 }
+
+Card.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  codeStack: PropTypes.string,
+};
 
 
